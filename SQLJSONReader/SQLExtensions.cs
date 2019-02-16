133ed6557446fa83890843b43af538ca0fdf9616
Wegerson <github_ww@wegerson.com>
2019-02-16 16:09:22 -0700

    Namespace and image

--- SQLJSONReader/SQLExtensions.cs
+++ SQLJSONReader/SQLExtensions.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 using Newtonsoft.Json;
 
-namespace SQLJSONReader
+namespace Omega.SQLJSON.Extensions
 {
     public static class SQLExtensions
     {
